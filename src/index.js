be557dbe29a670687a36db607bb5f917b21514f4
cnadjim <c.nadjim@gmail.com>
2023-09-18 18:40:21 +0200

    add landing page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
-import App from './app/App';
+import './assets/font/inter/inter.css';
 import store from "./app/store";
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
+import App from "./app/App";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
